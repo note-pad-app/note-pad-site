7c530dc48ca29de44e64efdb9a22a2bd2e3c2191
EdrissAria <edrissalokozay77@gmail.com>
2024-04-07 06:51:13 -0700

    update the view page of notes

--- src/components/catagoryCard/index.tsx
+++ src/components/catagoryCard/index.tsx
@@ -4,7 +4,7 @@ function CatagoryCard() {
   return (
     <div className="catagory-card d-flex justify-content-between align-items-center my-3 p-3 shadow-sm bg-white rounded">
         <div className="d-flex align-items-end">
-          <input type="text" value="Personal" />
+          <input className='category-input' type="text" value="Personal" />
         </div>
         <div>
           <i className="text-success me-4 fas fa-save fs-2"></i>
--- src/pages/Viewnote.tsx
+++ src/pages/Viewnote.tsx
@@ -1,27 +1,45 @@
+import { useState } from 'react';
 import CkEditor from '../components/ckeditor'
-const now = new Date().toDateString(); 
+const now = new Date().toDateString();
 function Viewnote() {
+  const [edit, setedit] = useState(false)
+
   return (
     <main>
       <div className="container pt-4 view-note">
-        <div className="view-titlebar d-flex justify-content-between align-items-center">
-          <div className='d-flex justify-content-start align-items-center'>
-            <i className="fas fa-clipboard fs-4 m-2"></i>
-            <select name="" id="" className='form-select form-select-sm border-0'>
-              <option selected>No catagory</option>
-              <option value="work">work</option>
-              <option value="personal">personal</option>
-              <option value="life">life</option>
-              <option value="home">home</option>
-            </select>
-          </div>
-          <p>{now}</p>
-        </div>
-        {/*ckeditor*/}
-        <div className="mt-4">
-          <CkEditor />
-        </div>
-        <button className='btn mt-3 btn-success rounded-3 px-4 py-2 shadow-sm'>Update</button>
+        {
+          edit ? <div>
+            <div className="view-titlebar d-flex justify-content-between align-items-center">
+              <div className='d-flex justify-content-start align-items-center'>
+                <select name="" id="" className='form-select form-select-sm border-0'>
+                  <option selected>No catagory</option>
+                  <option value="work">work</option>
+                  <option value="personal">personal</option>
+                  <option value="life">life</option>
+                  <option value="home">home</option>
+                </select>
+              </div>
+              <p>{now}</p>
+            </div>
+            {/*ckeditor*/}
+            <div className="mt-4">
+              <CkEditor />
+            </div>
+            <button onClick={()=> setedit(false)} className='btn mt-3 btn-success rounded-3 px-4 py-2 shadow-sm'>Save</button></div>
+            : <div>
+              <div>
+                <div className="view-titlebar d-flex justify-content-between align-items-center">
+                  <div className='d-flex justify-content-start align-items-center'>
+                    <h2 className='fs-4 text-info'>Work</h2>
+                  </div>
+                  <p>{now}</p>
+                </div>
+                <div className="mt-4 note-content">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto similique blanditiis velit numquam suscipit minus magnam non adipisci nobis perferendis cupiditate explicabo quidem fuga, iure totam iste in nemo animi!
+                </div>
+                <button onClick={()=> setedit(true)} className='btn mt-3 btn-primary rounded-3 px-4 py-2 shadow-sm'>Edit</button></div>
+            </div>
+        }
       </div>
     </main>
   )
